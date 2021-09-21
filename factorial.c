75ea83d202c34e8eb701d433aff58e5ee9873523
Alexey Gladckov <alexey.gladckov@gmail.com>
2021-09-21 17:41:04 +0300

    У Стёпы вонючее очко

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	long res = 1;
+	int end;
+
+	scanf("%d", &end);
+	for (int i = 2; i <= end; ++i)
+		res *= i;
+
+	printf("Result: %ld\n", res);
+	return 0;
+}
